8a665b8391c5b7bb65d5fe04b5078d1568129d5d
Van <1027154225@qq.com>
2023-08-23 14:53:39 +0800

    style(custom): 优化代码格式

--- src/pages/visualization/components/supplier-chart.vue
+++ src/pages/visualization/components/supplier-chart.vue
@@ -19,7 +19,7 @@ function init() {
     props.data.forEach((item) => {
         label.value.push(item.name);
         data.value.push(
-            Math.round(((item.num / top) * 40 + (item.avg / 5) * 60) * 10) / 10
+            Math.round(((item.num / top) * 40 + (item.avg / 5) * 60) * 10) / 10,
         );
     });
 
